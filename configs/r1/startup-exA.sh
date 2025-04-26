ab089d0ce01e021387c935df65b0c32976afcd68
Eduardo <edu.s1734@hotmail.com>
2025-04-26 19:06:19 +0100

    feat: ospf config for exA of routers

--- configs/r1/startup-exA.sh
+++ configs/r1/startup-exA.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-vtysh << EOF
-conf t
-ip route 10.10.11.0/24 10.10.99.3
-exit
-EOF
+# vtysh << EOF
+# conf t
+# ip route 10.10.11.0/24 10.10.99.3
+# exit
+# EOF
--- configs/r2/startup-exA.sh
+++ configs/r2/startup-exA.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-vtysh << EOF
-conf t
-ip route 10.10.10.0/24 10.10.99.10
-exit
-EOF
+# vtysh << EOF
+# conf t
+# ip route 10.10.10.0/24 10.10.99.10
+# exit
+# EOF
