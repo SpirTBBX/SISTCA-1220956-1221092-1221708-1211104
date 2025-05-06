1469c09f93b5533889bab6a2dc52c60e35e3f04b
Eduardo <edu.s1734@hotmail.com>
2025-05-06 17:17:04 +0100

    feat+fix: Fix endlines and added challenge docker files

---
+++ configs/runner/startup-challenge.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Give execution permissions to the startup script on all containers
+docker exec router1 chmod +x /etc/startup-challenge.sh
+docker exec router2 chmod +x /etc/startup-challenge.sh
+docker exec router3 chmod +x /etc/startup-challenge.sh
+docker exec router4 chmod +x /etc/startup-challenge.sh
+docker exec host chmod +x /etc/startup.sh
+docker exec srv chmod +x /etc/startup.sh
+
+# Execute the startup-tutorial script on all containers
+docker exec router1 /etc/startup-challenge.sh
+docker exec router2 /etc/startup-challenge.sh
+docker exec router3 /etc/startup-challenge.sh
+docker exec router4 /etc/startup-challenge.sh
+docker exec host /etc/startup.sh
+docker exec srv /etc/startup.sh
--- configs/runner/startup-exB.sh
+++ configs/runner/startup-exB.sh
@@ -3,7 +3,7 @@
 # Give execution permissions to the startup script on all containers
 docker exec router1 chmod +x /etc/startup-exB.sh
 docker exec router2 chmod +x /etc/startup-exB.sh
-docker exec router3 chmod +x /etc/startup-exA.sh
+docker exec router3 chmod +x /etc/startup-exB.sh
 docker exec router4 chmod +x /etc/startup-exB.sh
 docker exec host chmod +x /etc/startup.sh
 docker exec srv chmod +x /etc/startup.sh
@@ -11,7 +11,7 @@ docker exec srv chmod +x /etc/startup.sh
 # Execute the startup-tutorial script on all containers
 docker exec router1 /etc/startup-exB.sh
 docker exec router2 /etc/startup-exB.sh
-docker exec router3 /etc/startup-exA.sh
+docker exec router3 /etc/startup-exB.sh
 docker exec router4 /etc/startup-exB.sh
 docker exec host /etc/startup.sh
 docker exec srv /etc/startup.sh
