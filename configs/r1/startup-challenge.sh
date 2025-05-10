83a04ff382f7e35eff34cdbc05b39fd43bc72467
Eduardo <edu.s1734@hotmail.com>
2025-05-10 18:00:01 +0100

    feat: ospf cost configuration through interfaces

--- configs/r1/startup-challenge.sh
+++ configs/r1/startup-challenge.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+ROUTER3_IFACE=$(ip -o -4 addr show | awk '/10.10.99.2\// {print $2}')
+ROUTER4_IFACE=$(ip -o -4 addr show | awk '/10.10.99.18\// {print $2}')
+
+echo "Router1 interface to communicate with Router3: $ROUTER3_IFACE"
+echo "Router1 interface to communicate with Router4: $ROUTER4_IFACE"
+
 vtysh << EOF
 conf t
 hostname router1
@@ -11,6 +17,11 @@ network 10.10.10.0/24 area 0
 network 10.10.99.0/29 area 0
 network 10.10.99.16/29 area 0
 exit
+interface $ROUTER3_IFACE
+ip ospf cost 10
+interface $ROUTER4_IFACE
+ip ospf cost 30
 exit
 write memory
+exit
 EOF
